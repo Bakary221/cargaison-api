f7d9afe75fdae4b6e7a67799cc6e1fc7bee6a96e
Bakary <bakarydiassy28@gmail.com>
2025-08-17 06:29:35 +0000

    quelque modifs

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+// Middleware CORS
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // ou "http://localhost:8000"
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 server.use(middlewares);
 server.use(router);
 
